e472a82aa4161651b90e13f76d19a2a7b0d37db5
M ZEESHAN <106513298+Xeeshan85@users.noreply.github.com>
2024-04-20 16:17:14 +0500

    Update Palindrome.asm

--- Palindrome.asm
+++ Palindrome.asm
@@ -1,3 +1,9 @@
+
+; An assembly program that takes a string and check if it is a palindrome or not. If the string
+; is a palindrome, then the program stores P in a variable otherwise it stores N. For Example, "You're damn mad,
+; Roy" is not a palindrome. So N is stored in the variable
+
+
 .model small
 .stack 100h
 
@@ -11,30 +17,30 @@ MAIN PROC
     mov ds, ax
 
 
-    mov si, lengthof string
+    mov si, lengthof string ; Gets the length of the string
     dec si
 
     mov di, 0
     L1:
-        mov al, byte ptr [string+di]
-        mov bl, byte ptr [string+si]
+        mov al, byte ptr [string+di] ; Checks from start of the string
+        mov bl, byte ptr [string+si] ; Checks from the end of the string
 
         cmp al, bl
-        jne ExitLoop
+        jne ExitLoop ; If not Equal Exit the loop
 
-        cmp si, di
-        je PalindromExit
+        cmp si, di 
+        je PalindromExit ; If both si and di becomes equal then exit
 
-        inc di
+        inc di ; else increment di and si
         dec si
         jmp L1
 
     PalindromExit:
-        mov var, 050h
+        mov var, 050h ; Store P in the variable indicating the string is palindrome
 
     jmp EXIT
     ExitLoop:
-        mov var, 04Eh
+        mov var, 04Eh ; Store N in the variable indicating the string is not a palindrome
 
     EXIT:
         mov ax, 4Ch
