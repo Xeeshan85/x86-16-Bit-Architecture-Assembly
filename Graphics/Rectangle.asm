8eb1963e625bcea0560abe3b20a55bfc305c31bb
M ZEESHAN <106513298+Xeeshan85@users.noreply.github.com>
2024-04-21 17:57:32 +0500

    Update Rectangle.asm

--- Graphics/Rectangle.asm
+++ Graphics/Rectangle.asm
@@ -1,1 +1,41 @@
+; Program draws a rectangle full of pixels on the screen.
+
+.model small
+.stack 100h
+
+.data
+.code
+main proc
+    mov ax, @data
+    mov ds, ax
+
+    ; Video Mode
+    mov ah, 0
+    mov al, 13h
+    int 10h
+    
+    mov cx, 100
+    mov dx, 50
+    mov si, cx
+    mov di, 50
+
+	L1:
+		mov al, 110b
+		mov ah, 0ch
+		int 10h
+
+		inc cx
+		cmp cx, 200
+		jne L1
+
+		mov cx, si
+		inc dx
+		cmp dx, 100
+		jne L1
+
+		mov ah, 4ch
+		int 21h
+
+main endp
+end main
 
